ef939b4d60d36c0d5999e366700a47db14e9e01f
sharmaine1028 <54767477+sharmaine1028@users.noreply.github.com>
2022-07-09 17:10:45 +0800

    Create expo app with firbase config and colours

---
+++ my-app/config/colours.js
@@ -0,0 +1,10 @@
+/**
+ * App theme colours
+ */
+export default {
+  littleBoyBlue: "#809BCE",
+  paleCerulean: "#95B8D1",
+  middleBlueGreen: "B8E0D2",
+  honeyDew: "D6EADF",
+  cameoPink: "EAC4D5",
+};
---
+++ my-app/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import "firebase/app";
+import firestore from "firebase/firestore";
+import "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyCk12QbHLxalv81DeNakovAFe7wCyZfr0k",
+  authDomain: "lifehack-ab5fd.firebaseapp.com",
+  projectId: "lifehack-ab5fd",
+  storageBucket: "lifehack-ab5fd.appspot.com",
+  messagingSenderId: "821420703462",
+  appId: "1:821420703462:web:995e4461a1d46c70890527",
+  measurementId: "G-J7HV25NLQ8",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+export { auth, db };
